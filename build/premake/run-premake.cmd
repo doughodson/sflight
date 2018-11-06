e75bebccea7fa3a6a4ac083cf122c9370932e7c3
Doug Hodson <doug@sidechannel.net>
2018-11-06 12:21:04 -0500

    updated simpleflight premake script

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -1,9 +1,9 @@
 rem
 rem delete old solution/workspace files
 rem
-rmdir /S /Q ..\vs2013
-rmdir /S /Q ..\vs2015
+rmdir /S /Q ..\gmake
 rmdir /S /Q ..\vs2017
+rmdir /S /Q ..\obj
 rem
 rem delete old compiled libraries
 rem
@@ -11,6 +11,4 @@ del ..\..\lib\*.lib
 rem
 rem create solution/workspace files
 rem
-premake5-alpha11.exe --os=windows --file=premake5.lua vs2013
-premake5-alpha11.exe --os=windows --file=premake5.lua vs2015
 premake5-alpha11.exe --os=windows --file=premake5.lua vs2017
