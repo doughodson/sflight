df782cc5bcf08f6d86c8039ff598030f88ca4341
Doug Hodson <doug@sidechannel.net>
2018-11-06 12:52:34 -0500

    adding linux support for premake

---
+++ build/premake/run-premake.sh
@@ -0,0 +1,14 @@
+#
+# remove any generated build files
+#
+rm -rf ..\gmake
+rm -rf ..\vs2017
+rm -rf ..\obj
+#
+# remove any compiled libraries
+#
+rm -rf ..\..\lib\*.lib
+#
+# create new build files files
+#
+./premake5-alpha13 --os=linux --file=premake5.lua gmake
